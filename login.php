77af4386ba62dbe1f98cbfc94083fc92bc036e3f
Ikeda_Tomomi <abcd.y327.s2@gmail.com>
2022-05-16 12:40:48 +0900

    push

--- login.php
+++ login.php
@@ -8,28 +8,31 @@ try {
 
     $dbh = new PDO($dsn, $user, $password);
 
-    $name = $_POST["name"];
+    $mail = $_POST["mail"];
     $pass = $_POST["pass"];
 
-    $sql = 'SELECT * FROM members WHERE name=:name and password=:pass';
+    $sql = 'SELECT * FROM members WHERE mail_address=:mail and password=:pass';
 
     $stmt = $dbh->prepare($sql);
-    $stmt->bindValue(":name", $name);
+    $stmt->bindValue(":mail", $mail);
     $stmt->bindValue(":pass", $pass);
 
     $stmt->execute();
 
-    while ($member = $stmt->fetch(PDO::FETCH_ASSOC)) {
-        if ($name == $member["name"] && $pass == $member["password"]) {
-            //ログイン認証　成功
-            $_SESSION["name"] = $name;
-            header("Location:top.php");
-            exit;
-        } else if ($name != $member["name"] || $pass != $member["password"]) {
-            //ログイン認証　失敗
-            header("Location:error.html");
-            exit;
-        }
+    // while ($member = $stmt->fetch(PDO::FETCH_ASSOC)) {
+    if ($member = $stmt->fetch(PDO::FETCH_ASSOC)) {
+        //ログイン認証　成功
+
+        // パスワード情報のみ、セッションから削除（プライバシーの点により）
+        unset($member["password"]);
+        $_SESSION["member"] = $member;
+        header("Location:top.php");
+        // exit;
+    // } else if ($mail != $member["mail_address"] || $pass != $member["password"]) {
+    }else{    
+        //ログイン認証　失敗
+        header("Location:error.html");
+        // exit;
     }
 } catch (PDOException $e) {
     print("DB Connection Error!<br>" . $e->getMessage());

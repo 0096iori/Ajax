31954bd2f58ac8eb9951fa38ee307a098898fb3a
kuga3 <20011109@c.anabuki-college.net>
2022-06-13 11:42:59 +0900

    tophtml改良

--- top.php
+++ top.php
@@ -40,10 +40,26 @@
 
                     <!--コンテンツ領域-->
                     <div id="top_mainContent">
-                        <p>--コンテンツ領域--</p>
+                        <form action="" method="get" class="search_div">
+                            <input type="search" name="keyword" placeholder="店名、キーワード、時間帯" class="key">
+                            <input type="submit" class="input_btn" value="検索" class="search_btn">
+                        </form>
+                        <div id="favorite">
+                            <a href="" class="input_btn top_btn">お気に入り</a>
+                        </div>
+                        <div>
+                            <h4>おすすめのお店</h4>
+                            <div id="recommendation">
+                                <a href=""><img src="image/fd401527.jpg" alt="" class="photo"></a>
+                                <a href=""><img src="image/beaf2.jpg" alt="" class="photo"></a>
+                                <a href=""><img src="image/bread.jpg" alt="" class="photo"></a>
+                                <a href=""><img src="image/chinese-st.jpg" alt="" class="photo"></a>
+                                <a href=""><img src="image/cokkie.jpg" alt="" class="photo"></a>
+                                <a href=""><img src="image/fried-salmon.jpg" alt="" class="photo"></a>
+                            </div>
+                        </div>
                     </div>
                 </div>
-
             <?php
             } else {
             ?>

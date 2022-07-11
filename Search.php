7acc82607112f35a2c7849d8e32953e2428d156f
0096iori <102918434+0096iori@users.noreply.github.com>
2022-07-11 10:41:14 +0900

    CSS_未完

--- Search.php
+++ Search.php
@@ -8,7 +8,7 @@
 </head>
 <body>
 <header>
-    <link href="style.css"media="all" rel="stylesheet">
+    <link href="css/style.css"media="all" rel="stylesheet">
     <script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
 </header>
